bfdcfc583618a035348e2ffa6aa4fccbd8d76c56
Nat <nat@cmmc.io>
2017-04-03 17:26:28 +0700

    Add dependencies.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import CLI from 'clui'
 import chalk from 'chalk'
 import clear from 'clear'
 import figlet from 'figlet'
@@ -5,36 +6,57 @@ import _ from 'underscore'
 import Preferences from 'preferences'
 import { getCredentials } from './questions'
 import { login } from './netpie'
+
 import inquirer from 'inquirer'
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const pref = new Preferences('config')
 
-clear()
-console.log(
-  chalk.magenta(
-    figlet.textSync('netpie.js-cli', {horizontalLayout: 'full'})
+let prompLogin = (processed) => {
+  return inquirer.prompt(
+    {
+      type: 'list',
+      name: 'Actions',
+      message: 'What do you want to do?',
+      choices: [
+        'Create new app',
+        new inquirer.Separator(), ...processed
+      ]
+    }
   )
-)
+}
 
-getCredentials((...args) => {
-  args[0].username = 'nat.wrw@gmail.com'
-  args[0].password = 'UoTR8IG19oh7'
-  pref.netpie = {username: args.username, password: args.password}
-  login(...args, (args2) => {
-    let j = JSON.parse(args2)
-    let processed = _.map(j, (v, k) => { return v.appid })
-    inquirer.prompt(
-      {
-        type: 'list',
-        name: 'Actions',
-        message: 'What do you want to do?',
-        choices: [
-          'Create new app',
-          new inquirer.Separator(), ...processed
-        ]
-      }
+let doLoginPrompt = () => {
+  clear()
+  console.log(
+    chalk.magenta(
+      figlet.textSync('netpie.js-cli', {horizontalLayout: 'full'})
     )
+  )
+  getCredentials((...args) => {
+    // args[0].username = 'xnat.wrw@gmail.com'
+    // args[0].password = 'UoTR8IG19oh7'
+    var status = new CLI.Spinner('Authenticating you, please wait...')
+    status.start()
+    pref.netpie = {username: args.username, password: args.password}
+    login(...args).then((args2) => {
+      status.stop()
+      let j = JSON.parse(args2)
+      let processed = _.map(j, (v, k) => v.appid)
+      prompLogin(processed).then(() => {
+        console.log('ok')
+      })
+    })
+    .then((...args) => {
+      console.log('then ..', ...args)
+    })
+    .catch(() => {
+      status.stop()
+      console.log('invalid login')
+      doLoginPrompt()
+    })
   })
-})
+}
+
+doLoginPrompt()
--- src/netpie.js
+++ src/netpie.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const parse = (html, type) => {
   return new Promise((resolve, reject) => {
-    var text
+    let text
     let $ = cheerio.load(html)
     if (type !== Constants.TYPE_APP_LIST) {
       if (type !== Constants.TYPE_APP_DETAIL) {
@@ -42,47 +42,52 @@ const get = (url) => {
   })
 }
 
-const getAppList = (request, reply) => {
-  get('https://netpie.io/app', Constants.TYPE_APP_LIST)
-  .then(parseAppList)
-  .then(JSON.parse)
-  .then((json) => {
-    return extend({}, {apps: json})
-  })
-  .then((object) => {
-    const tasks = _.collect(object.apps.app, (app) => {
-      return get('https://netpie.io/app/' + app.appid, Constants.TYPE_APP_DETAIL)
+const getAppList = (request) => {
+  return new Promise((resolve, reject) => {
+    get('https://netpie.io/app', Constants.TYPE_APP_LIST)
+    .then(parseAppList)
+    .then(JSON.parse)
+    .then((json) => {
+      return extend({}, {apps: json})
     })
-    return Promise.all(tasks)
-  })
-  .then((apps) => {
-    return new Promise((resolve, reject) => {
-      const tasks = _.collect(apps, (content) => {
-        return parseAppDetail(content).then(JSON.parse)
+    .then((object) => {
+      const tasks = _.collect(object.apps.app,
+        (app) => get('https://netpie.io/app/' + app.appid, Constants.TYPE_APP_DETAIL))
+      return Promise.all(tasks)
+    })
+    .then((apps) => {
+      return new Promise((resolve, reject) => {
+        const tasks = _.collect(apps, (content) => parseAppDetail(content).then(JSON.parse))
+        resolve(tasks)
       })
-      resolve(tasks)
     })
+    .then((tasks) => Promise.all(tasks))
+    .then(JSON.stringify)
+    .then((...args) => resolve(...args))
+    .catch(reject)
   })
-  .then((tasks) => {
-    return Promise.all(tasks)
-  })
-  .then(JSON.stringify)
-  .then(reply)
 }
 
-export let login = (request, reply) => {
-  requestAgent.post('https://netpie.io:443/actions/login')
-  .redirects(5)
-  .type('form')
-  // .send(request.params)
-  .send({username: request.username, password: request.password, redirectpath: ''})
-  .end(function (err, res) {
-    if (!(err || !res.ok)) {
-      requestAgent.saveCookies(res)
-      getAppList(request, reply)
-    } else {
-      console.log('Oh no! error ', err)
-      console.log('Oh no! error ', err)
-    }
+export let login = (request, callback) => {
+  return new Promise((resolve, reject) => {
+    requestAgent.post('https://netpie.io:443/actions/login')
+    .redirects(5)
+    .type('form')
+    // .send(request.params)
+    .send({username: request.username, password: request.password, redirectpath: ''})
+    .end(function (err, res) {
+      if (!(err || !res.ok)) {
+        requestAgent.saveCookies(res)
+        // getAppList(request, (...args) => { resolve(callback(...args)) })
+        console.log('must be resolved')
+        getAppList(request, (cb) => {
+          console.log('must be resolved 2')
+          resolve(cb)
+        }).catch(reject)
+      } else {
+        console.log('Oh no! error ', err)
+        reject(err)
+      }
+    })
   })
 }
